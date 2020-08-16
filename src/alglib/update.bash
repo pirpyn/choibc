ffd6e46b898a0627871793019b3d8572557d177e
pirpyn <payen.pierre@gmail.com>
2020-08-16 19:17:02 +0200

    Get ALGLIB for optimisation

---
+++ src/alglib/update.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+dir=$(dirname $(readlink -m $0))
+cd $dir
+# Go to https://www.alglib.net/download.php and get url
+url="https://www.alglib.net/translator/re/alglib-3.16.0.cpp.gpl.tgz"
+wget $url
+tar xvf alglib-*.tgz
+files=(optimization ap alglibinternal linalg alglibmisc solvers )
+for file in ${files[@]}; do
+    cp cpp/src/$file.* .
+done
+rm $dir/*.tgz $dir/*.tgz.*
+rm -rf $dir/cpp
