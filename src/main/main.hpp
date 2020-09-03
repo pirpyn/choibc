d1602d0609c948813fad46b74594fa780abbac20
pirpyn <payen.pierre@gmail.com>
2020-09-03 09:08:37 +0200

    Moved write impedances to external file

--- src/main/main.hpp
+++ src/main/main.hpp
@@ -1,10 +1,6 @@
 #ifndef _H_MAIN
 #define _H_MAIN
 
-#include "../hoibc/hoibc.hpp"
-#include "read_json.hpp"
-#include <string>
-
-void write_impedance_errors(const data_out_t& data_out, std::vector<hoibc::hoibc_class*>& hoibc_list);
+int main(int argc, char* argv[]);
 
 #endif
---
+++ src/main/write_impedance_errors.hpp
@@ -0,0 +1,9 @@
+#ifndef _H_WRITE_IMPEDANCE_ERRORS
+#define _H_WRITE_IMPEDANCE_ERRORS
+
+#include "../hoibc/hoibc.hpp"
+#include "read_json.hpp"
+
+void write_impedance_errors(const data_out_t& data_out, std::vector<hoibc::hoibc_class*>& hoibc_list);
+
+#endif
