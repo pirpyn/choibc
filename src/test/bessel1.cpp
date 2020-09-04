43643f7a8aee790236ce74906a72d8b3468cfc45
pirpyn <payen.pierre@gmail.com>
2020-09-04 11:39:00 +0200

    test for bessel

---
+++ src/test/bessel1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "../hoibc/hoibc_bessel.hpp"
+#include "../hoibc/hoibc_types.hpp"
+#include <limits>
+
+#define CHECK(label) if (abs(computed-reference) < 10.*std::numeric_limits<double>::epsilon()) { \
+        cout << "[OK]   " << label << ", error = " << abs(computed - reference) << endl; \
+    } else { \
+        cerr << "[FAIL] " << label << ", error = " << abs(computed - reference) << endl; \
+        no_of_errors++;\
+    }
+
+using namespace std;
+using namespace hoibc;
+
+int main(){
+  cout << "Testing bessel1" << endl;
+  int no_of_errors {0};
+  hoibc::complex reference;
+  hoibc::complex computed;
+  
+  reference = 0.765197686557966551449717526102663220909274289755325241861;
+  computed  = bessel1(0.,hoibc::complex(1.,0.));
+  CHECK("bessel1(0.,1.)   ");
+
+  reference = 0.019985850304223122424228390950848990680633578859027929558;
+  computed  = bessel1(0.,hoibc::complex(100.,0.));
+  CHECK("bessel1(0.,100.) ");
+
+  reference = 2.6306151236874532069978536877905029440885704143207273E-10;
+  computed  = bessel1(10.,hoibc::complex(1.,0.));
+  CHECK("bessel1(10.,1.)  ");
+
+  reference = 0.096366673295861559674314024870401848311755419825021855917;
+  computed  = bessel1(100.,hoibc::complex(100.,0.));
+  CHECK("bessel1(10.,100.)");
+
+  return no_of_errors;
+
+}
---
+++ src/test/bessel2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "../hoibc/hoibc_bessel.hpp"
+#include "../hoibc/hoibc_types.hpp"
+#include <limits>
+
+#define CHECK(label) if (abs(computed-reference) < 10.*std::numeric_limits<double>::epsilon()) { \
+        cout << "[OK]   " << label << ", error = " << abs(computed - reference) << endl; \
+    } else { \
+        cerr << "[FAIL] " << label << ", error = " << abs(computed - reference) << endl; \
+        no_of_errors++;\
+    }
+
+using namespace std;
+using namespace hoibc;
+
+int main(){
+  cout << "Testing bessel2" << endl;
+  int no_of_errors {0};
+  hoibc::complex reference;
+  hoibc::complex computed;
+  
+  reference = hoibc::complex(0.7651976865579665514497175261026632209092742897553252418, -0.08825696421567695798292676602351516282781752309067554671);
+  computed  = bessel2(0.,hoibc::complex(1.,0.));
+  CHECK("bessel2(0.,1.)  ");
+
+  reference = hoibc::complex(2.630615123687453206997853687791E-10,+1.2161801427868918928813042666797E8);
+  computed  = bessel2(10.,hoibc::complex(1.,0.));
+  CHECK("bessel2(10.,1.) ");
+
+  reference = hoibc::complex(-0.4400505857449335159596822037189,-0.7812128213002887165471500000480);
+  computed  = bessel2p(0.,hoibc::complex(1.,0.));
+  CHECK("bessel2p(0.,1.) ");
+
+  reference = hoibc::complex(2.618635056224421836032912981021E-9,-1.2093999378481599097712034732764E9);
+  computed = bessel2p(10.,hoibc::complex(1.,0.));
+  CHECK("bessel2p(10.,1.)");
+
+  return no_of_errors;
+
+}
