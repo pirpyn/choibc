c78b2e8742b13c52d00ce291da8211228fc779a6
pirpyn <payen.pierre@gmail.com>
2020-11-06 08:49:48 +0100

    improved alglib script

--- src/alglib/update.bash
+++ src/alglib/update.bash
@@ -4,12 +4,14 @@ dir=$(dirname $(readlink -m $0))
 
 # Go to https://www.alglib.net/download.php and get url
 last_url="https://www.alglib.net/translator/re/alglib-3.16.0.cpp.gpl.tgz"
-url="$(curl -s https://www.alglib.net/download.php | awk '/<a.*\.cpp\.gpl\.tgz/ { print gensub(".*href=\"(.*)\".*","https://www.alglib.net\\1","g",$0);}')"
-if [[ $last_url == $url ]]; then
+url=$(curl -s https://www.alglib.net/download.php | awk '/<a.*\.cpp\.gpl\.tgz/ { print gensub(".*href=\"(.*)\".*","https://www.alglib.net\\1","g",$0);}')
+if [[ "$last_url" == "$url" && "$1" != force ]]; then
     echo "No update available"
     exit
 else
-    sed -i "s|$last_url|$url|" $(readlink -m $0)
+    if [[ -n "${url}" ]]; then
+        sed -i "s|^last_url=.*|last_url=\"${url}\"|" $(readlink -m $0)
+    fi
     cd $dir
     wget $url
 fi
@@ -17,7 +19,7 @@ fi
 tar xf alglib-*.tgz
 files=(optimization ap alglibinternal linalg alglibmisc solvers stdafx)
 for file in ${files[@]}; do
-    echo "alglib: optimize: $file"
+    echo "  updating: $file"
     cp cpp/src/$file.* .
     sed -i 's/\r//g' $file.*
 done
