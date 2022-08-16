5d6042557d678ef2015b99e556f05b171fb9b889
Pirpyn <payen.pierre@gmail.com>
2022-08-17 00:19:15 +0200

    Merged main and write impedance errors

--- src/main/write_impedance_errors.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef _H_WRITE_IMPEDANCE_ERRORS
-#define _H_WRITE_IMPEDANCE_ERRORS
-
-#include "../hoibc/hoibc.hpp"
-
-void write_impedance_errors(const data_out_t& data_out, std::vector<hoibc::hoibc_class*>& hoibc_list);
-
-#endif
